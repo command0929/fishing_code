e3dad4179d6e0c49ca39b1163bed8a7b4a61025c
command0929 <142138531+command0929@users.noreply.github.com>
2023-08-15 20:56:46 +0900

    Update info.js

--- global_modules/info.js
+++ global_modules/info.js
@@ -1,2 +1,29 @@
 /* 라이센스 없음.
 어캐 쓰는 자유. 정지 책임 안짐 */
+var command = {
+ play: ['ㄴㅅ',",낚시"],
+ heal: ['ㅎㅅ', ",휴식"],
+ rank: ['ㄹㅋ', ",랭킹"],
+ shop: ['ㅅㅈ', ",상점"],
+ rebirth: ['ㅎㅅ', ",환생"],
+ myinfo: ['ㄴㅈㅂ', ",내정보"],
+ sell: ['ㅁㄱㄱㅍㅁ', ",물고기판매"],
+ shop2: ['ㅅㅈ2', ",상점2"],
+ place: ['ㅈㅅ', ',장소'],
+ buy: ['ㄱㅁ', ";구매"],
+ join: ['ㄱㅇ', ",가입"]
+};
+exports.cmd = command;
+
+exports.place = {
+
+},
+
+exports.shop = {
+    first: {
+
+    },
+    second: {
+
+    }
+}
